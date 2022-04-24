db4d1d926cd7bec959ef062d0e3eed65af38e491
shigobu <shigobu@shigobu.com>
2022-04-24 21:26:56 +0900

    プロジェクト ファイルを追加します。

---
+++ BitmapFontViewer/Model/DelegateCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace BitmapFontViewer
+{
+    /// <summary>
+    /// プリズムのコードを参考に、デリゲートコマンドを作成。
+    /// </summary>
+    class DelegateCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public DelegateCommand(Action executeMethod)
+        {
+            ExecuteMethod = executeMethod;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            ExecuteMethod?.Invoke();
+        }
+
+        private Action ExecuteMethod { get; set; }
+    }
+}
---
+++ BitmapFontViewer/ViewModel/ViewModel.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace BitmapFontViewer
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChangedの実装
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+          => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        #endregion
+
+        public ViewModel(MainWindow mainWindow)
+        {
+            Owner = mainWindow;
+            OpenBitmapCommand = new DelegateCommand(OpenBitmap);
+            UpdateCommand = new DelegateCommand(UpdateBitmap);
+        }
+
+        #region プロパティ
+
+        /// <summary>
+        /// mainWindow
+        /// </summary>
+        public MainWindow Owner { get; set; }
+
+        private string _BitmapFileName = "bitmap未指定";
+        /// <summary>
+        /// bitmapファイル名
+        /// </summary>
+        public string BitmapFileName
+        {
+            get => _BitmapFileName;
+            set
+            {
+                if (_BitmapFileName == value) return;
+                _BitmapFileName = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        private int _FontWidth = 6;
+        /// <summary>
+        /// フォント幅
+        /// </summary>
+        public int FontWidth
+        {
+            get => _FontWidth;
+            set
+            {
+                if (_FontWidth == value) return;
+                _FontWidth = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private int _FontHeight = 8;
+        /// <summary>
+        /// フォント高さ
+        /// </summary>
+        public int FontHeight
+        {
+            get => _FontHeight;
+            set
+            {
+                if (_FontHeight == value) return;
+                _FontHeight = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string _SearchChar = "";
+        /// <summary>
+        /// 検索文字
+        /// </summary>
+        public string SearchChar
+        {
+            get => _SearchChar;
+            set
+            {
+                if (_SearchChar == value) return;
+                _SearchChar = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private int[] _magnificationComboItem = {1, 2, 3, 4};
+        /// <summary>
+        /// コンボボックスアイテム
+        /// </summary>
+        public int[] MagnificationComboItem
+        {
+            get => _magnificationComboItem;
+            set
+            {
+                if (_magnificationComboItem == value) return;
+                _magnificationComboItem = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private int _Magnification = 1;
+        /// <summary>
+        /// 倍率
+        /// </summary>
+        public int Magnification
+        {
+            get => _Magnification;
+            set
+            {
+                if (_Magnification == value) return;
+                _Magnification = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        private BitmapSource? _CharBitmap;
+        public BitmapSource? CharBitmap
+        {
+            get => _CharBitmap;
+            set
+            {
+                if (_CharBitmap == value) return;
+                _CharBitmap = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion プロパティ
+
+        #region コマンド
+
+        /// <summary>
+        /// bitmap指定ボタンコマンド
+        /// </summary>
+        public ICommand OpenBitmapCommand { get; private set; }
+
+        public ICommand UpdateCommand { get; private set; }
+
+        #endregion
+
+        #region コマンドメソッド
+
+        private void OpenBitmap()
+        {
+            OpenFileDialog fileDialog = new();
+            fileDialog.Filter = "画像ファイル|*.BMP;*.GIF;*.EXIF;*.JPG;*.PNG;*.TIFF";
+
+            if (fileDialog.ShowDialog() ?? false)
+            {
+                BitmapFileName = fileDialog.FileName;
+            }
+        }
+
+        /// <summary>
+        /// フォント画像を更新します。
+        /// </summary>
+        private void UpdateBitmap()
+        {
+            if (string.IsNullOrWhiteSpace(BitmapFileName))
+            {
+                return;
+            }
+
+            if (!System.IO.File.Exists(BitmapFileName))
+            {
+                return;
+            }
+
+            if (FontWidth == 0 || FontHeight == 0)
+            {
+                return;
+            }
+
+            if (Magnification == 0)
+            {
+                return;
+            }
+
+            BitmapImage fontBitmap = new BitmapImage(new Uri(BitmapFileName));
+            CroppedBitmap croppedBitmap = new CroppedBitmap(fontBitmap, new Int32Rect(30, 20, 105, 50));
+
+            CharBitmap = croppedBitmap; 
+        }
+
+        #endregion コマンドメソッド
+
+        #region メソッド
+
+
+
+        #endregion
+
+    }
+}
